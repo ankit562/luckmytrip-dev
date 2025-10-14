b05d70e55d93f10a766399a3a1dda52a96e67e55
Ritik Kesarwani <kesarwaniraj11@gmail.com>
2025-10-15 02:49:20 +0530

    add cros changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,9 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+  origin: process.env.NODE_ENV === 'production' ? process.env.CORS_ORIGIN : "http://localhost:5173",
+  credentials: true
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "2mb"}))
