eb4f0e35cf0e2b83cf324f827b414a6383b91fab
Ritik Kesarwani <kesarwaniraj11@gmail.com>
2025-10-15 00:42:30 +0530

    changes in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['cookie'], // Add 'cookie' to external dependencies
+    },
+  },
   resolve: {
     alias: {
       '@': '/src',
