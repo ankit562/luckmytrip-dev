e2480ec11c2003c834d878703e139c9661dc6f02
ankit562 <ankitprajapati562@gmail.com>
2025-11-12 00:45:33 +0530

    homepage
    homepage content udpated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -292,9 +292,9 @@ const HomePage = () => {
           <div className="container mx-auto px-4 relative z-10">
             <div className="mb-4">
               <p className="text-4xl text-center font-bold py-2">
+                Buy a ₹599 ticket, win a luxury holiday to Dubai or Thailand
               </p>
               <div className="text-center py-6 mb-16 pb-16">
-                <p className="text-base md:text-xl font-medium font-montserrat">Buy a ₹599 ticket, win a luxury holiday to Dubai or Thailand</p>
                 <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-3 px-6 rounded-md w-48">
                   Click Here
                 </button>
